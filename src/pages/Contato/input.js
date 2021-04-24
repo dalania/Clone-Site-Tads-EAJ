b0d53820abfab141a6d37cbd2f8b6cde7d3f069f
dalania silva <jdalaniasilva@gmail.com>
2021-04-23 22:28:25 -0300

    removido comentario

--- src/pages/Contato/input.js
+++ src/pages/Contato/input.js
@@ -1,7 +1,7 @@
 import React,{useEffect, useRef} from 'react';
 import { useField}  from '@unform/core';
 
-{/* responsavel por conectar o input com o unform */ }
+
 export default function Input({name, ...rest}){
   const inputRef = useRef(null);
   const {fieldName,registerField, defaultValue, error } = useField(name);
